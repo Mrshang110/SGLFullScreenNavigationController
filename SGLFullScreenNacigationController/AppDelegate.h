45143066ed9461ba452640c59054f5c7ae4fda44
Mr.Shang.110 <shanggl@missfresh.cn>
2016-07-22 19:49:22 +0800

    初始提交

---
+++ SGLFullScreenNacigationController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SGLFullScreenNacigationController
+//
+//  Created by 爱上党 on 16/7/22.
+//  Copyright © 2016年 尚高林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SGLFullScreenNacigationController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SGLFullScreenNacigationController
+//
+//  Created by 爱上党 on 16/7/22.
+//  Copyright © 2016年 尚高林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
